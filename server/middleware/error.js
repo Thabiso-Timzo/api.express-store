2a4881420117712846ae2096e7cb0923b4590280
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-20 20:30:19 +0200

    Modified error file

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -10,6 +10,24 @@ const errorCatcher= (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
     }
 
+    // Duplicate key error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Wrong Jwt error
+    if (err.code === "jsonWebTokenError") {
+        const message = `Your url is invalid, please try again.`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    // Jwt expired error
+    if (err.code === "TokenExpiredError") {
+        const message = `Your url has expired, please try again.`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
