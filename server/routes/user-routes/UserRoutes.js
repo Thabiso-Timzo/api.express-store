78cdfd199ceba941ce7d8016ad3f85ac5c467e53
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-21 16:06:42 +0200

    Added user routes

---
+++ server/routes/user-routes/UserRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const { 
+    createUser, 
+    loginUser,
+    logoutUser,
+    forgotPassword,
+    resetPassword,
+    userDetails,
+    updatePassword,
+    updateProfile,
+    getAllUsers,
+    getSingleUser,
+    updateUserRole,
+    deleteUser
+} = require('../../controller/user-controller/UserController');
+const { isAuthenticatedUser, authorisedRoles } = require('../../middleware/auth');
+
+router.post('/register', createUser);
+router.post('/login', loginUser);
+router.post('/password/forgot', forgotPassword );
+
+router.get('/logout', logoutUser);
+router.get('/me',isAuthenticatedUser, userDetails);
+router.get('/admin/users', isAuthenticatedUser, authorisedRoles('admin'), getAllUsers);
+router.get('/admin/users/:id', isAuthenticatedUser, authorisedRoles('admin'), getSingleUser);
+
+router.put('/password/reset/:token', resetPassword);
+router.put('/me/update', isAuthenticatedUser, updatePassword);
+router.put('/me/update/info', isAuthenticatedUser, updateProfile);
+router.put('/admin/users/:id', isAuthenticatedUser, authorisedRoles('admin'), updateUserRole);
+
+router.delete('/admin/users/:id', isAuthenticatedUser, authorisedRoles('admin'), deleteUser);
+
+module.exports = router;
