39c6aff3676b041417b41e9f809c077f5f83553c
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-23 18:57:00 +0200

    Modifeid navigation bar

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,36 +1,37 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-//import { useSelector, useDispatch } from 'react-redux'
-//import { logout, reset } from '../redux/auth/authSlice'
+import { Link, useNavigate } from 'react-router-dom'
+import { BsHeart, BsCart3, BsSearch, BsChatDots } from 'react-icons/bs'
+import { CgProfile } from 'react-icons/cg'
+import { BiLogOut } from 'react-icons/bi'
 
 import './NavBar.css'
 import logo from '../../images/logo.png'
 
 const NavigationBar = () => {
     const [show, setShow] = useState(true);
+    const navigate = useNavigate();
 
-    //const history = useHistory();
-    //const dispatch = useDispatch();
-    //const { user } = useSelector((state) => state.auth);
-
-    function showSwitch() {
+    const showSwitch = () => {
         return setShow(!show);
     }
 
-    // const onLogout = () => {
-    //     dispatch(logout());
-    //     dispatch(reset());
-    //     history.push('/')
-    // }
+    const logoLink = () => {
+        navigate('/products')
+    }
 
   return (
     <div className="navbar"> 
         <div className="logo">
-            <img src={logo} alt=""/>
+            <img onClick={logoLink} src={logo} alt=""/>
         </div>
-        <div className={show ? "links active" : "links"}> 
-            <Link onClick={() => showSwitch()} to="/">Login</Link>
-            <Link onClick={() => showSwitch()} to="/register">Register</Link>
+        <div className={show ? "links active" : "links"}>
+            <button>Sell on Gude</button>
+            <Link onClick={() => showSwitch()} to="/search"><BsSearch size={25} /></Link> 
+            <Link onClick={() => showSwitch()} to="/message"><BsChatDots size={25} /></Link> 
+            <Link onClick={() => showSwitch()} to="/favourites"><BsHeart size={25} /></Link>
+            <Link onClick={() => showSwitch()} to="/cart"><BsCart3 size={25} /></Link>
+            <Link onClick={() => showSwitch()} to="/profile"><CgProfile size={25} /></Link>
+            <i className='logout'><BiLogOut size={25} /></i>
         </div>
         <div 
             className={show ? "bars-button active" : "bars-button"} 
