a7829ec276132a481f29ffd10c20b093f4861c6a
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-23 18:54:26 +0200

    Modified App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import NavBar from './components/navbar/NavBar';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
+import Login from './pages/authentication/login/Login';
+import Register from './pages/authentication/register/Register';
+import Cart from './pages/cart/Cart';
+import Favourites from './pages/favourites/Favourites';
+import Home from './pages/home/Home';
+import Message from './pages/message/Message';
+import Profile from './pages/profile/Profile';
+import Search from './pages/search/Search';
 
 function App() {
   return (
     <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" exact element={<Login/>} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Login/>} /> 
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
