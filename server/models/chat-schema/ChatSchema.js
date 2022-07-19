07653e33fec7ea180db26ce0a63933dbeec47d96
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-07-19 17:20:32 +0200

    Added a chat svhema

---
+++ server/models/chat-schema/ChatSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const chatSchema = mongoose.Schema({
+    members: {
+        type: Array,
+    }
+}, { timestamps: true });
+
+const Chat = mongoose.model('Chat', chatSchema);
+module.exports = Chat;
