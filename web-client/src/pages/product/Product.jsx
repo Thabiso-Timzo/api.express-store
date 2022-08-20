664161047af1ab719efdbd9fe6480c4ec196dcfa
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-08-20 12:17:29 +0200

    added Empty pages

---
+++ web-client/src/pages/product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Product.css'
+import Navigation from '../../components/navigation/Navigation'
+
+const Product = () => {
+  return (
+    <>
+        <Navigation />
+    </>
+  )
+}
+
+export default Product
---
+++ web-client/src/pages/wish-list/WishList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './WishList.css'
+import Navigation from '../../components/navigation/Navigation'
+
+const WishList = () => {
+  return (
+    <>
+        <Navigation />
+    </>
+  )
+}
+
+export default WishList
