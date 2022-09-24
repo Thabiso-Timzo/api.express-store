0a637170a1eba3721d20eb548cab24e1544b09bf
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-09-24 21:38:01 +0200

    Modified user model

--- server/models/user-schema/UserSchema.js
+++ server/models/user-schema/UserSchema.js
@@ -1,89 +1,72 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcrypt')
+const crypto = require("crypto");
+const { jwt_secret, jwt_exp } = require('../../config/index');
 
 const userSchema = new mongoose.Schema({
     name:{
-        type:String,
-        required: true,
+        type: String,
+        required: [true, "Please enter your full name!"],
     },
     email:{
-       type:String,
-       validate: [validator.isEmail,"Please enter a valid email"],
-       unique: true,
-   },
-   password:{
-      type:String,
-      required:true,
-   },
-//    avatar:{
-//     public_id:{
-//         type:String,
-//         required:true,
-//     },
-//     url:{ 
-//         type:String,
-//         required:true,
-//     },
-//    },
-   role:{
-       type:String,
-       default: "user",
-   },
-   createdAt:{
-     type: Date,
-     default:Date.now(),
-   },
-   resetPasswordToken: String,
-   resetPasswordTime: Date,
+       type: String,
+        required: [true, "Please enter your email!"],
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: [true,"Please enter your password!"]
+    },
+    avatar:{
+        type: String,
+        default: "https://res.cloudinary.com/du1lcyerz/image/upload/v1663846874/profile-icon-png-898_mxfyyf.png"
+    },
+    role:{
+        type: Number,
+        default: 0,
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date
+}, {
+    timestamps: true
 });
-
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-      next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-  
-//Verify password
-userSchema.methods.isPasswordMatch = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
 
 // Hash password
-/*userSchema.pre("save", async function(next){
-     if (!this.isModified("password")) {
-        next();
-      }
-    this.password = await bcrypt.hash(this.password,10);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
 });
-
+  
 // jwt token
-userSchema.methods.getJwtToken = function(){
-    return jwt.sign({id:this._id}, jwt_secret, {
-        expiresIn: jwt_exp
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id }, jwt_secret, {
+      expiresIn: jwt_exp,
     });
 };
-
+  
 // compare password
-userSchema.methods.comparePassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password);
-}
-
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+  
 // Forgot password
-userSchema.methods.getResetToken = function(){
+userSchema.methods.getResetToken = function () {
     // Generating token
-   const resetToken = crypto.randomBytes(20).toString("hex");
-    
-//    hashing and adding resetPasswordToken to userSchema
-this.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
-
-this.resetPasswordTime = Date.now() + 15 * 60 * 1000;
-
-return resetToken;
-}*/
+    const resetToken = crypto.randomBytes(20).toString("hex");
+  
+    //    hashing and adding resetPasswordToken to userSchema
+    this.resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
+  
+    this.resetPasswordTime = Date.now() + 15 * 60 * 1000;
+  
+    return resetToken;
+};
 
 const User = mongoose.model("User",userSchema);
 module.exports = User;
