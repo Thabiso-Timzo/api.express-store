1c2cb745302fc90e6601c8a33302959b743d4436
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-09-25 15:54:37 +0200

    Modified file

--- web-client/src/components/profile-dropdown/ProfileDropDown.jsx
+++ web-client/src/components/profile-dropdown/ProfileDropDown.jsx
@@ -1,49 +1,68 @@
 import React from 'react'
-import  { Link } from 'react-router-dom'
+import  { Link, useNavigate } from 'react-router-dom'
 import { MdLogout, MdLogin, MdAppRegistration } from 'react-icons/md'
 import { CgProfile } from 'react-icons/cg'
+import { useSelector, useDispatch } from 'react-redux'
 
 import './ProfileDropDown.css'
 import defImag from '../../assets/images/profile.png'
+import { logout, reset } from '../../features/auth/authSlice'
 
 const UserProfile = ({ open }) => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+
+    const logoutHandler = () => {
+        dispatch(logout());
+        dispatch(reset())
+        navigate('/')
+    };
+
+    //const { userInfo } = state;
   return (
     <div className="profile-menu">
         {open ? <div className="profile-profile-menu">
-             <div className="profile-prof">
-                 <img src={defImag} alt='' />
-                 <div className="info">
-                     <h2>Thabiso Hlatshwayo</h2>
-                 </div>
-             </div>
-             <ul>
-                 <li>
-                    <Link to="/profile" className="profile-btn">
-                        <CgProfile className="profile-icon"/>
-                        Profile
-                    </Link>
-                 </li>
-                 <li>
-                    <Link to="/login" className="profile-btn">
-                        <MdLogin className="profile-icon"/>
-                        Login
-                    </Link>
-                 </li>
-                 <li>
-                    <Link to="/register" className="profile-btn">
-                        <MdAppRegistration className="profile-icon"/>
-                        Register
-                    </Link>
-                 </li>
-                 <li>
-                     <button className="profile-btn">
-                        <MdLogout className="profile-icon" />
-                         Logout
-                    </button>
-                 </li>
-             </ul>
-             
-        </div>: null }
+            {user ? (
+                <div className="profile-prof">
+                    <img src={defImag} alt='' />
+                    <div className="info">
+                        <h2>{user.name}</h2>
+                    </div>
+                </div>
+            ) : null}
+            {user ? (
+                <ul>
+                    <li>
+                        <Link to="/profile" className="profile-btn">
+                            <CgProfile className="profile-icon"/>
+                            Profile
+                        </Link>
+                    </li>
+                    <li>
+                        <button className="profile-btn" onClick={logoutHandler}>
+                            <MdLogout className="profile-icon" />
+                            Logout
+                        </button>
+                    </li>
+                </ul>
+            ) : (
+                <ul>
+                    <li>
+                        <Link to="/login" className="profile-btn">
+                            <MdLogin className="profile-icon"/>
+                            Login
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/register" className="profile-btn">
+                            <MdAppRegistration className="profile-icon"/>
+                            Register
+                        </Link>
+                    </li>
+                </ul>
+                )}
+        </div> : null}
     </div>
   )
 }
