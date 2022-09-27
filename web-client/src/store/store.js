2057dbba98866661382ac82404d5f582a64a957e
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-09-27 13:25:18 +0200

    Updated the store page

--- web-client/src/store/store.js
+++ web-client/src/store/store.js
@@ -1,33 +1,78 @@
-import { createStore, compose, applyMiddleware } from "redux";
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-//import { composeWithDevTools } from "redux-devtools-extension";
- 
-import { reducer }  from "./index";
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  deleteProductReducer,
+  deleteReviewReducer,
+  newProductReducer,
+  newReviewReducer,
+  productDetailsReducer,
+  productReviewsReducer,
+  productsReducer,
+} from "../reducers/product-reducers/productReducers";
+import { 
+  allUsersReducer, 
+  forgotPasswordReducer, 
+  profileReducer, 
+  userDetailsReducer, 
+  userReducer 
+} from "../reducers/user-reducers/UserReducers";
+import { cartReducer } from "../reducers/cart-reducers/cartReducers";
+import { favouriteReducer } from "../reducers/wish-reducers/wishReducers";
+import {
+  allOrdersReducer,
+  myOrdersReducer,
+  newOrderReducer,
+  orderDetailsReducer,
+  orderReducer,
+} from "../reducers/order-reducers/orderReducers";
 
-const userAuthFromStorage = localStorage.getItem('userAuthData')
-  ? JSON.parse(localStorage.getItem('userAuthData'))
-  : null;
+const reducer = combineReducers({
+  products: productsReducer,
+  productDetails: productDetailsReducer,
+  user: userReducer,
+  profile: profileReducer,
+  cart: cartReducer,
+  favourite: favouriteReducer,
+  order: newOrderReducer,
+  myOrder: myOrdersReducer,
+  myOrderDetails: orderDetailsReducer,
+  newReview: newReviewReducer,
+  createProduct: newProductReducer,
+  deleteProduct: deleteProductReducer,
+  AllOrders: allOrdersReducer,
+  deleteOrder: orderReducer,
+  allUsers: allUsersReducer,
+  userDetails: userDetailsReducer,
+  deleteReview: deleteReviewReducer,
+  productReviews: productReviewsReducer,
+  forgotPassword:forgotPasswordReducer,
+});
 
 let initialState = {
-  userLogin: { 
-    userInfo: userAuthFromStorage 
+  cart: {
+    cartItems: localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : [],
+
+    shippingInfo: localStorage.getItem("shippingInfo")
+      ? JSON.parse(localStorage.getItem("shippingInfo"))
+      : {},
+  },
+  favourite: {
+    favouriteItems: localStorage.getItem("favouriteItems")
+      ? JSON.parse(localStorage.getItem("favouriteItems"))
+      : [],
   },
 };
 
 const middleWare = [thunk];
 
-const init_reduce = () => {
-  return (
-    initialState,
-    reducer
-  )
-}
-
 const store = createStore(
-  init_reduce,
-  composeEnhancer(applyMiddleware(...middleWare)),
-  //composeWithDevTools(applyMiddleware(...middleWare))
-)
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
 
-export default store;  
+export default store;
