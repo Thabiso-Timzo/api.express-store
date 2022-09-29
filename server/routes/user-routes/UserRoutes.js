b08f7a9f8bcef7fdc659c2e12ca1b8e2deca778b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-09-29 17:45:40 +0200

    Modified user routes

--- server/routes/user-routes/UserRoutes.js
+++ server/routes/user-routes/UserRoutes.js
@@ -2,37 +2,40 @@ const express = require('express');
 const router = express.Router();
 
 const { 
+    auth,
     createUser, 
     loginUser,
     logoutUser,
-    forgotPassword,
-    resetPassword,
-    userDetails,
-    updatePassword,
-    updateProfile,
-    getAllUsers,
-    getSingleUser,
-    updateUserRole,
-    deleteUser,
-    checkIfStudent,
-    getMe
+    // forgotPassword,
+    // resetPassword,
+    // userDetails,
+    // updatePassword,
+    // updateProfile,
+    // getAllUsers,
+    // getSingleUser,
+    // updateUserRole,
+    // deleteUser,
+    // checkIfStudent,
 } = require('../../controller/user-controller/UserController');
-const { isAuthenticatedUser, authorisedRoles } = require('../../middleware/auth');
+const authUser  = require('../../middleware/auth');
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
-router.post('/password/forgot', forgotPassword );
-router.post('/check', authorisedRoles, checkIfStudent)
+// router.post('/password/forgot', forgotPassword );
+// router.post('/check', authorisedRoles, checkIfStudent);
 
-router.get('/me',isAuthenticatedUser, userDetails);
-router.get('/admin/users', isAuthenticatedUser, authorisedRoles(1), getAllUsers);
-router.get('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), getSingleUser);
+router.get('/auth', authUser, auth);
 
-router.put('/password/reset/:token', resetPassword);
-router.put('/me/update', isAuthenticatedUser, updatePassword);
-router.put('/me/update/info', isAuthenticatedUser, updateProfile);
-router.put('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), updateUserRole);
+router.get('/logout', authUser, logoutUser);
+// router.get('/me',isAuthenticatedUser, userDetails);
+// router.get('/admin/users', isAuthenticatedUser, authorisedRoles(1), getAllUsers);
+// router.get('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), getSingleUser);
 
-router.delete('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), deleteUser);
+// router.put('/password/reset/:token', resetPassword);
+// router.put('/me/update', isAuthenticatedUser, updatePassword);
+// router.put('/me/update/info', isAuthenticatedUser, updateProfile);
+// router.put('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), updateUserRole);
+
+// router.delete('/admin/user/:id', isAuthenticatedUser, authorisedRoles(1), deleteUser);
 
 module.exports = router;
