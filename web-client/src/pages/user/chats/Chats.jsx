d912ed772cd04cba15647701cb44e20fd3c07e60
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-09-30 19:42:30 +0200

    Modified chats file

--- web-client/src/pages/user/chats/Chats.jsx
+++ web-client/src/pages/user/chats/Chats.jsx
@@ -5,30 +5,28 @@ import './Chats.css'
 import Navigation from '../../../components/navigation/Navigation'
 
 const Chats = () => {
-  const { user } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.user)
   return (
     <>
       <Navigation />
       {user ? (
-        <div className="Chat">
-        <div className="Left-side-chat">
-          <div className="Chat-container">
-            <h2>Chats</h2>
-            <div className="Chat-list">
-            
-              Conversations
-            </div>
-          </div>
-        </div>
-
-        <div className="Right-side-chat">
-          Right side
-        </div>
-      </div>
-      ): (
         <div className="unauthorised">
           <h1>Unauthorised</h1>
           <p>You need to login in order you for to access this page</p>
+        </div>
+      ): (
+        <div className="Chat">
+          <div className="Left-side-chat">
+            <div className="Chat-container">
+              <h2>Chats</h2>
+              <div className="Chat-list">
+                Conversations
+              </div>
+            </div>
+          </div>
+          <div className="Right-side-chat">
+            Right side
+          </div>
         </div>
       )}  
     </>
