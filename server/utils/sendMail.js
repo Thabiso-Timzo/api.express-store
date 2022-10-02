260ef01911780e9331f9ba0cba86dc5de9df5baf
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-02 19:42:23 +0200

    Changed sendMail functionality

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,32 +1,64 @@
-const nodeMailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+const  { google } = require('googleapis');
+const { OAuth2 } = google.auth;
+const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground/';
 
-const { 
-  smpt_host, 
-  smpt_port,
-  smpt_service,
-  smpt_mail,
-  smpt_pass
+const {
+  mailing_id,
+  mailing_secret,
+  mailing_refresh,
+  smpt_mail
 } = require('../config/index');
 
-const sendMail = async (options) => {
-    const transporter = nodeMailer.createTransport({
-      host: smpt_host,
-      port: smpt_port,
-      service: smpt_service,
-      auth: {
-        user: smpt_mail,
-        pass: smpt_pass,
-      },
-    });
-  
-    const mailOptions = {
-      from: smpt_mail,
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
-    };
-  
-    await transporter.sendMail(mailOptions);
-  };
-  
-  module.exports = sendMail;
+const oauth2Client = new OAuth2(
+  mailing_id,
+  mailing_secret,
+  mailing_refresh,
+  smpt_mail,
+  OAUTH_PLAYGROUND
+)
+
+const sendEmail = (to, url, txt) => {
+  oauth2Client.setCredentials({
+    refresh_token: mailing_refresh
+  })
+
+  const accessToken = oauth2Client.getAccessToken();
+  const smtpTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: smpt_mail,
+      clientId: mailing_id,
+      clientSecret: mailing_secret,
+      refreshToken: mailing_refresh,
+      accessToken
+    } 
+  });
+
+  const mailOptions = {
+    from: smpt_mail,
+    to: to,
+    subject: "Gude",
+    html: `
+      <div>
+        <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Gude.</h2>
+        <p>Congratulations! You're almost set to start using Gude.
+          Just click the button below to validate your email address.
+        </p>
+    
+        <a href=${url} style="background: red; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+
+        <p>If the button doesn't work for any reason, you can also click on the link below:</p>
+
+        <div>${url}</div>
+      </div>  
+    `
+  }
+  smtpTransport.sendMail(mailOptions, (err, infor) => {
+    if (err) return err;
+    return infor
+  })
+}
+
+module.exports = sendEmail;
