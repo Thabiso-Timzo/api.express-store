329bf87464ac3d1def1e3e704be504dba8cd60d3
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-03 11:45:55 +0200

    Modified user modal

--- server/models/user-schema/UserSchema.js
+++ server/models/user-schema/UserSchema.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const jwt = require('jsonwebtoken');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type:String,
-        maxlength:50
+        required: [true, "Please enter your name."],
+        trim: true
     },
     email: {
         type:String,
+        required: [true, "Please enter your email address."],
         trim:true,
-        unique: 1 
+        unique: true 
     },
     password: {
         type: String,
-        minglength: 5
+        required: [true, "Please enter your password."],
     },
     role : {
         type:Number,
@@ -25,62 +24,9 @@ const userSchema = mongoose.Schema({
         type: String,
         default: "https://res.cloudinary.com/du1lcyerz/image/upload/v1663846874/profile-icon-png-898_mxfyyf.png"
     } ,
-    token : {
-        type: String,
-    },
-    tokenExp :{
-        type: Number
-    }
+}, {
+    timestamps: true
 })
-
-
-userSchema.pre('save', function( next ) {
-    var user = this;
-    
-    if(user.isModified('password')){    
-
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
-    
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err);
-                user.password = hash 
-                next()
-            })
-        })
-    } else {
-        next()
-    }
-});
-
-userSchema.methods.comparePassword = function(plainPassword,cb){
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err);
-        cb(null, isMatch)
-    })
-}
-
-userSchema.methods.generateToken = function(cb) {
-    var user = this;
-    var token =  jwt.sign(user._id.toHexString(),'secret')
-
-    user.token = token;
-    user.save(function (err, user){
-        if(err) return cb(err)
-        cb(null, user);
-    })
-}
-
-userSchema.statics.findByToken = function (token, cb) {
-    var user = this;
-
-    jwt.verify(token,'secret',function(err, decode){
-        user.findOne({"_id":decode, "token":token}, function(err, user){
-            if(err) return cb(err);
-            cb(null, user);
-        })
-    })
-}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User 
