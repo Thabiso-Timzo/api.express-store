cf43b32ae1b5a85a4798441bc01039350c23a575
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-04 20:29:10 +0200

    Modified index.js file

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import store from './store/store';
+
+import DataProvider from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <DataProvider>
     <App />
-  </Provider>
+  </DataProvider>
 );
