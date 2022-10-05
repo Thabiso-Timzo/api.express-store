ac62a317551e350529a729e3b3cfeb6a5280863c
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-05 19:15:02 +0200

    Modified store file

--- web-client/src/store/store.js
+++ web-client/src/store/store.js
@@ -1,72 +1,37 @@
 // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import {
-  deleteProductReducer,
-  deleteReviewReducer,
-  newProductReducer,
-  newReviewReducer,
-  productDetailsReducer,
-  productReviewsReducer,
-  productsReducer,
-} from "../reducers/product-reducers/productReducers";
-import { allUsersReducer, forgotPasswordReducer, profileReducer, userDetailsReducer, userReducer } from "../reducers/user-reducers/UserReducers";
-import { cartReducer } from "../reducers/cart-reducers/cartReducers";
-import { favouriteReducer } from "../reducers/wish-reducers/wishReducers";
-import {
-  allOrdersReducer,
-  myOrdersReducer,
-  newOrderReducer,
-  orderDetailsReducer,
-  orderReducer,
-} from "../reducers/order-reducers/orderReducers";
+import { createStore  } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from '../reducers/index'
 
-const reducer = combineReducers({
-  products: productsReducer,
-  productDetails: productDetailsReducer,
-  user: userReducer,
-  profile: profileReducer,
-  cart: cartReducer,
-  favourite: favouriteReducer,
-  order: newOrderReducer,
-  myOrder: myOrdersReducer,
-  myOrderDetails: orderDetailsReducer,
-  newReview: newReviewReducer,
-  createProduct: newProductReducer,
-  deleteProduct: deleteProductReducer,
-  AllOrders: allOrdersReducer,
-  deleteOrder: orderReducer,
-  allUsers: allUsersReducer,
-  userDetails: userDetailsReducer,
-  deleteReview: deleteReviewReducer,
-  productReviews: productReviewsReducer,
-  forgotPassword:forgotPasswordReducer,
-});
+// let initialState = {
+//   cart: {
+//     cartItems: localStorage.getItem("cartItems")
+//       ? JSON.parse(localStorage.getItem("cartItems"))
+//       : [],
 
-let initialState = {
-  cart: {
-    cartItems: localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
-      : [],
+//     shippingInfo: localStorage.getItem("shippingInfo")
+//       ? JSON.parse(localStorage.getItem("shippingInfo"))
+//       : {},
+//   },
+//   favourite: {
+//     favouriteItems: localStorage.getItem("favouriteItems")
+//       ? JSON.parse(localStorage.getItem("favouriteItems"))
+//       : [],
+//   },
+// };
 
-    shippingInfo: localStorage.getItem("shippingInfo")
-      ? JSON.parse(localStorage.getItem("shippingInfo"))
-      : {},
-  },
-  favourite: {
-    favouriteItems: localStorage.getItem("favouriteItems")
-      ? JSON.parse(localStorage.getItem("favouriteItems"))
-      : [],
-  },
-};
-
-const middleWare = [thunk];
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleWare))
-);
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-export default store;
+function DataProvider({children}) {
+  return (
+      <Provider store={store}>
+          {children}
+      </Provider>
+  )
+}
+
+export default DataProvider;
