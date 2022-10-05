5b8dc86471bb7bd9a08668efa294aaff5c192f74
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-05 18:58:43 +0200

    Modified navigation

--- web-client/src/components/navigation/Navigation.jsx
+++ web-client/src/components/navigation/Navigation.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { FiSearch } from 'react-icons/fi'
 import { HiOutlineChat } from 'react-icons/hi'
 import { BsHeart, BsShop } from 'react-icons/bs'
 import { GiShoppingCart } from 'react-icons/gi'
 import { MdOutlineSell } from 'react-icons/md'
-//import { useSelector } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { IoPersonCircleOutline } from 'react-icons/io5'
 
 import './Navigation.css'
 import img1 from '../../assets/logo/Gude.png'
-import profile from '../../assets/images/profile.png'
 import UserProfile from '../profile-dropdown/ProfileDropDown'
-import defImag from '../../assets/images/profile.png'
 
 const Navigation = () => {
     const [open, setOpen] = useState(false)
@@ -22,7 +20,9 @@ const Navigation = () => {
     // Navigate to the products page
     const home = () => navigate('/')
 
-    const { user } = useSelector((state) => state.user)
+    const auth = useSelector(state => state.auth)
+    
+    const { user, isLogged } = auth
 
     // Check if its a student or a seller
     const sellOnGude = async (e) => {
@@ -34,28 +34,31 @@ const Navigation = () => {
         <nav className='top-nav'>
             <img onClick={home} src={img1} alt="" />
             <ul className='top-ul'>
-                {user ? null : (<li><button onClick={sellOnGude}><MdOutlineSell /> sell on gude</button></li>)}
+                {isLogged ?  <li><button onClick={sellOnGude}><MdOutlineSell /> sell on gude</button></li> : null}
                 <li><Link to='/search'><FiSearch /></Link></li>
-                {user ? null : (<li><Link to='/chat'><HiOutlineChat /></Link></li>)}
+                {isLogged ? <li><Link to='/chat'><HiOutlineChat /></Link></li> : null}
                 <li><Link to='/wish-list'><BsHeart /></Link></li>
                 <li><Link to='/cart'><GiShoppingCart /></Link></li>
                 <li>
                     <div className="profile-action" onClick={() => setOpen(!open)}>
                         <UserProfile  open={open} />
-                        <img src={defImag} alt=''/>
+                        {isLogged ? <img src={user.avatar} alt=''/> : <IoPersonCircleOutline  size={23}/>}
                     </div>
                 </li> 
             </ul>
         </nav>
         
-        <nav className={!user ? 'bottom-nav' : 'no-user'}>
+        <nav className={isLogged ? 'bottom-nav' : 'no-user'}> 
             <ul className='bottom-ul'>
                 <li><Link to='/'><BsShop /></Link></li>
-                {user ? null : (<li><Link to='/chat'><HiOutlineChat /></Link></li>)}
-                {user ? null : (<li><Link to='#' onClick={sellOnGude}><MdOutlineSell /></Link></li>)}
+                {isLogged ? <li><Link to='/chat'><HiOutlineChat /></Link></li> : null}
+                {isLogged ? <li><Link to='#' onClick={sellOnGude}><MdOutlineSell /></Link></li> : null}
                 <li><Link to='/wish-list'><BsHeart /></Link></li>
                 <li><Link to='/cart'><GiShoppingCart /></Link></li>
-                <li><Link to='/profile'><img src={profile} alt='' /></Link></li>
+                <li><Link to='/profile'>
+                        {isLogged ? <img src={user.avatar} alt=''/> : <IoPersonCircleOutline  size={23}/>}
+                    </Link>
+                </li>
             </ul>
         </nav>
     </>
