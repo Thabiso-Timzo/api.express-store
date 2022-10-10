6e47b2f3e5efe8546199a6d24af409ffa3fa0a73
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-10-10 16:30:30 +0200

    Updated product route

--- server/routes/product-routes/ProductRoutes.js
+++ server/routes/product-routes/ProductRoutes.js
@@ -2,27 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    allProducts, 
-    createProduct,
-    updateProducts,
-    deleteProduct,
-    singleProduct,
-    createProductReview,
-    getSingleProductReviews,
-    deleteReview
+    allProducts, createProduct, updateProducts,
+    deleteProduct, singleProduct, createProductReview,
+    getSingleProductReviews, deleteReview
 } = require('../../controller/product-controller/ProductController');
-const { isAuthenticatedUser, authorisedRoles } = require('../../middleware/auth');
+const auth = require('../../middleware/auth');
+const authAdmin = require('../../middleware/authAdmin');
 
-router.post('/new', isAuthenticatedUser, createProduct);
-router.post('/review', isAuthenticatedUser, createProductReview);
+router.post('/new', auth, createProduct);
+router.post('/review', auth, createProductReview);
 
 router.get('/', allProducts);
 router.get('/:id', singleProduct);
 router.get('/review', getSingleProductReviews);
 
-router.put('/:id', isAuthenticatedUser, authorisedRoles('admin'),updateProducts);
+router.put('/:id', auth, authAdmin, updateProducts);
 
-router.delete('/:id', isAuthenticatedUser, authorisedRoles('admin'),deleteProduct);
-router.delete('/all/reviews', isAuthenticatedUser, authorisedRoles('admin'), deleteReview);
+router.delete('/:id', auth, authAdmin, deleteProduct);
+router.delete('/all/reviews', auth, authAdmin, deleteReview);
 
 module.exports = router;
