b3083411e6fb66dbabcc6820689c70f9dac8bce5
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-11-11 21:18:53 +0200

    Updated profile page

--- web-client/src/pages/user/profile/Profile.jsx
+++ web-client/src/pages/user/profile/Profile.jsx
@@ -8,12 +8,21 @@ import profile from '../../../assets/images/profile.png'
 import store from '../../../assets/images/stock.jpg'
 import Modal from '../../../components/profile-modal/Modal'
 import Navigation from '../../../components/navigation/Navigation'
+import Student from '../../../components/student-profile/Student'
+import Information from '../../../components/user-information/Information'
+import Sold from '../../../components/sold-items/Sold'
+import About from '../../../components/about/About'
 
 const Profile = () => {
-  const [openModal, setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false)
+  const [togglestate, SetTogglestate] = useState(1)
 
   const user = useSelector((state) => state.userLogin)
   const { userInfo } = user
+
+  const toggleTab = (index) => {
+    SetTogglestate(index)
+  }
 
   return (
     <>
@@ -27,6 +36,41 @@ const Profile = () => {
            open={openModal} 
            onClose={() => setOpenModal(false)}
         />
+      </div>
+      <hr />
+      <div className="toggle-wrapper">
+        <div className='toggle-text'>
+          <div
+            className={togglestate === 1 ? "tab-active" : "tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            Student
+          </div>
+          <div
+            className={togglestate === 2 ? "tab-active" : "tabs"}
+            onClick={() => toggleTab(2)}
+          >
+            Information
+          </div>
+            <div 
+              className={togglestate === 3 ? "tab-active" : "tabs"}
+              onClick={() => toggleTab(3)}
+            >
+              Sold
+            </div>
+            <div 
+              className={togglestate === 4 ? "tab-active" : "tabs"}
+              onClick={() => toggleTab(4)}
+            >
+              About
+            </div>
+        </div>
+        <div>
+          {togglestate === 1 && <Student />}
+          {togglestate === 2 && <Information />}
+          {togglestate === 3 && <Sold />}
+          {togglestate === 4 && <About />}
+        </div>
       </div>
     </>
   )
