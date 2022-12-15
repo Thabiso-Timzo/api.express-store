72e444e1d9badc5bf4f7d28087cd102d36c88f2b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-15 13:51:12 +0200

    Modified chat file

--- client/src/pages/user/chats/Chats.jsx
+++ client/src/pages/user/chats/Chats.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux"
 
 import './Chats.css'
 import Navigation from '../../../components/navigation/Navigation'
-import { utiliseChat } from '../../../api/chatRequest'
+import { userChats } from '../../../api/chatRequest'
 import Conversation from '../../../components/conversation/Conversation'
 
 const Chats = () => {
@@ -15,45 +15,38 @@ const Chats = () => {
   useEffect(() => {
     const getChats = async () => {
       try {
-        const {data} = await utiliseChat(user._id)
+        const {data} = await userChats(userInfo._id)
         setChats(data)
-        console.log(data)
       } catch (error) {
         console.log(error)
       }
     }
     getChats()
-  }, [user]) 
+  }, [userInfo ]) 
 
   return (
     <>
       <Navigation />
-      {userInfo ? (
-        <div className="Chat">
-           <div className="Left-side-chat">
-              <div className="Chat-container">
-                <h2>Chats</h2>
-                <div className="Chat-list">
-                  {chats.map((chat, index) => {
-                    return (
-                      <div key={index}>
-                        <Conversation data={chat} currentUserId={user._id} />
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
+      <div className="top"></div>
+      <div className="Chat">
+        <div className="Left-side-chat">
+          <div className="Chat-container">
+            <h2>Chats</h2>
+            <div className="Chat-list">
+              {chats.map((chat, index) => {
+                return (
+                  <div key={index}>
+                    <Conversation data={chat} currentUserId={userInfo._id} />
+                  </div>
+                )
+              })}
+            </div>
           </div>
-          <div className="Right-side-chat">
-            Right side
-          </div>
-        </div> 
-      ) : (
-        <div className="unauthorised">
-          <h1>Unauthorised</h1>
-          <p>You need to login in order you for to access this page</p>
         </div>
-      )}  
+        <div className="Right-side-chat">
+          Right side
+        </div>
+      </div>  
     </>
   )
 }
