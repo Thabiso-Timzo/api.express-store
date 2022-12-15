41c3ad4c9865a4fe6a8aadf837665174b475489f
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-12-15 19:51:58 +0200

    updated forgot password ui

--- client/src/pages/auth/forgot-password/ForgotPassword.jsx
+++ client/src/pages/auth/forgot-password/ForgotPassword.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './ForgotPassword.css'
 import image2 from '../../../assets/landing page/1.png'
 //3:28:53
 const ForgotPassword = () => {
+    const [email, setEmail] = useState('')
   return (
     <section>
-    <div className="image-box">
+    <div className="forgot-image-box">
         <img src={image2} alt="" />
     </div>
-    <div className="content-box">
-        <div className="form-box">
+    <div className="forgot-content-box">
+        <div className="forgot-form-box">
             <h2>Password Recovery</h2>
             <form>
-                <div className="input-box">
+                <div className="forgot-input-box">
                     <span>Email</span>
                     <input 
                         type="text" 
                         id="name" 
                         name="name" 
-                        //value={name} 
+                        value={email} 
                         placeholder="Enter your email" 
-                        //onChange={onChange}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
-                <div className="input-box">
+                <div className="forgot-input-box">
                     <button>Send</button>
                 </div>
             </form>
