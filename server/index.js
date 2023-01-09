3915bb82e21cae2368ac6a8e752619fb37b289b2
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-01-09 15:49:48 +0200

    Modified index.js file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const cookParser = require('cookie-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+// 2:18:20
 require('colors')
 require('dotenv').config()
 
@@ -9,6 +12,7 @@ const { errorHandler } = require('./middleware/errorHandler')
 const dbConnection = require('./config/connection/database')
 const authRoutes = require('./routes/user-routes/authRoutes')
 
+// Server port
 const PORT = server_port || 8080
 
 const app = express()
@@ -19,6 +23,9 @@ dbConnection()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({  extended: true}))
+app.use(cookParser()) 
+app.use(cors());
+app.use(morgan('tiny'))
 
 // Middleware
 app.use(errorHandler)
