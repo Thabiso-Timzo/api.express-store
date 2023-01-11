06068e21f7f413b6fab0a96fdd28fda47327c857
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-01-11 16:00:32 +0200

    Modified index.js file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cookParser = require('cookie-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-// 2:18:20
+
 require('colors')
 require('dotenv').config()
 
@@ -11,6 +11,7 @@ const { server_port } = require('./config/env')
 const { errorHandler } = require('./middleware/errorHandler')
 const dbConnection = require('./config/connection/database')
 const authRoutes = require('./routes/user-routes/authRoutes')
+const productRoutes = require('./routes/product-routes/productRoutes')
 
 // Server port
 const PORT = server_port || 8080
@@ -32,6 +33,7 @@ app.use(errorHandler)
 
 // Routes
 app.use('/api/user', authRoutes)
+app.use('/api/product', productRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`.white.bold)
