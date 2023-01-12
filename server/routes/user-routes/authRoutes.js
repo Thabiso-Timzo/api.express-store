9c3d6a5759e3774339af02fd682305358f240130
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-01-12 22:10:53 +0200

    Updated auth routes

--- server/routes/user-routes/authRoutes.js
+++ server/routes/user-routes/authRoutes.js
@@ -11,12 +11,17 @@ const {
     blockUser,
     unblockUser,
     handleRefreshToken,
-    logout
+    logout,
+    updatePassword,
+    forgotPasswordToken,
+    resetPassword
 } = require('../../controllers/user-controller/userController')
 const { authMiddleWare, isAdmin } = require('../../middleware/authMiddleware')
 
 router.post('/register', register)
 router.post('/login', login)
+router.post('/forgot-password-token', forgotPasswordToken)
+
 
 router.get('/refresh', handleRefreshToken)
 router.get('/logout', logout)
@@ -25,8 +30,12 @@ router.get('/:id' ,authMiddleWare, getSingleUser)
 
 router.delete('/:id', deleteSingleUser)
 
+router.put('/reset-password/:token', resetPassword)
+router.put('/update', authMiddleWare,updatePassword)
 router.put('/update_user', authMiddleWare, updateUser)
 router.put('/blocked_user/:id', authMiddleWare, isAdmin, blockUser)
 router.put('/unblocked_user/:id', authMiddleWare, isAdmin, unblockUser)
 
 module.exports = router
+
+// 4:15:00
