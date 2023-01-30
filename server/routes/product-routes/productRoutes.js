d7c7026ebec526f6a5d36ae7a4fca67e7631a9fb
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-01-30 14:24:47 +0200

    Modified product routes

--- server/routes/product-routes/productRoutes.js
+++ server/routes/product-routes/productRoutes.js
@@ -6,15 +6,20 @@ const {
     getSingleProduct,
     getAllProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    addToWishList,
+    rating
 } = require('../../controllers/product-controller/productController')
 const { isAdmin, authMiddleWare  } = require('../../middleware/authMiddleware')
 
 router.post('/create', authMiddleWare, isAdmin, createProduct)
 
+// test wishlist
+router.put('/wishlist', authMiddleWare,  addToWishList)
+router.put('/rating', authMiddleWare, rating)
 router.put('/:id' ,authMiddleWare, isAdmin, updateProduct)
 
-router.get('/:id', getSingleProduct)
+router.get('/:id', getSingleProduct) 
 router.get('/', getAllProducts)
 
 router.delete('/:id',authMiddleWare, isAdmin, deleteProduct)
