e953e23c304cd51b6a008a3f5329d72cc5d7de39
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-01-31 13:55:58 +0200

    Modified index.js file

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const blogRoutes = require('./routes/blog-routes/blogRoutes')
 const categoryRoutes = require('./routes/category-routes/categoryRoutes')
 const blogCategoryRoutes = require('./routes/blog-category-routes/blogCategoryRoutes')
 const brandRoutes = require('./routes/brand-routes/brandRoutes')
+const couponRoutes = require('./routes/coupon-routes/couponRoutes')
 
 // Server port
 const PORT = server_port || 8080
@@ -42,6 +43,7 @@ app.use('/api/blog', blogRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/blog/category', blogCategoryRoutes)
 app.use('/api/brand', brandRoutes)
+app.use('/api/coupon', couponRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`.white.bold)
