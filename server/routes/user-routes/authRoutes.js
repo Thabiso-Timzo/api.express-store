f9972e028f0a038d3405af11aa3e995e12c922da
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-06 14:51:52 +0200

    Updated auth routes

--- server/routes/user-routes/authRoutes.js
+++ server/routes/user-routes/authRoutes.js
@@ -17,22 +17,37 @@ const {
     resetPassword,
     Adminlogin,
     getWishList,
-    saveAddress
+    saveAddress,
+    userCart,
+    getUserCart,
+    emptyCart,
+    applyCoupon,
+    createOrder,
+    getOrders,
+    updateOrderStatus
 } = require('../../controllers/user-controller/userController')
 const { authMiddleWare, isAdmin } = require('../../middleware/authMiddleware')
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/admin-login', Adminlogin)
+router.post('/cart', userCart)
 router.post('/forgot-password-token', forgotPasswordToken)
+router.post('/cart/apply-coupon', authMiddleWare, applyCoupon)
+router.post('/cart/cash-order', authMiddleWare, createOrder)
 
 router.get('/refresh', handleRefreshToken)
 router.get('/logout', logout)
 router.get('/all', getAllUsers)
+router.get('/cart', authMiddleWare, getUserCart)
 router.get('/wishlist', authMiddleWare, getWishList)
+router.get('/get-orders', authMiddleWare, getOrders)
 router.get('/:id' ,authMiddleWare, getSingleUser)
+
+router.put('/order/update-order/:id', authMiddleWare, isAdmin, updateOrderStatus)
 router.put('/reset-password/:token', resetPassword)
 
+router.delete('/empty-cart', authMiddleWare, emptyCart)
 router.delete('/:id', deleteSingleUser)
 
 router.put('/save-address', authMiddleWare, saveAddress)
