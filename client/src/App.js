b1cd6b104db32c5c57d7b212670b9c23cf091690
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-10 18:34:26 +0200

    Updated file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css';
+import './App.css'
 
-import Layout from './components/layout/Layout';
-import About from './screens/about/About';
-import Contact from './screens/contact/Contact';
-import Home from './screens/home/Home';
+import Layout from './components/layout/Layout'
+import About from './screens/about/About'
+import Contact from './screens/contact/Contact'
+import Home from './screens/home/Home'
+import Products from './screens/products/Products'
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
           <Route index element={<Home />} />
           <Route path='about' element={<About />}/>
           <Route path='contact' element={<Contact />}/>
+          <Route path='products' element={<Products />}/>
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
