5eed8e5e6de44896822f29b2f6ecfc23407e0a36
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-11 23:29:55 +0200

    Modified

--- client/src/screens/products/Products.jsx
+++ client/src/screens/products/Products.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { BiSearch } from 'react-icons/bi'
 
 const Products = () => {
   return (
-    <div className={'flex'}>
-          <input 
-            className={'outline-none border-none rounded-l-lg h-7 w-75 placeholder-gray-500 placeholder-opacity-100'} 
-            type="text" placeholder='search for products' 
-          />
-          <span 
-            className={'cursor-pointer bg-slate-400 rounded-r-lg h-7 w-8 p-1.5 text-[#fff]'}
-          >
-            <BiSearch />
-          </span>
-        </div>
+    <div>
+        products 
+    </div>
   )
 }
 
