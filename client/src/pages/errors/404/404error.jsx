fe65c60a34e5a3e8e239ac17b59e50bc9e87c778
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-20 10:01:31 +0200

    Added pages

---
+++ client/src/pages/errors/404/404error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div>
+      Not found
+    </div>
+  )
+}
+
+export default Error404
---
+++ client/src/pages/errors/405/405error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const error405 = () => {
+  return (
+    <div>
+      Not authenticated
+    </div>
+  )
+}
+
+export default error405
