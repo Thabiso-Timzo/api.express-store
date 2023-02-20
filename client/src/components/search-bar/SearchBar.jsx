2ddb3e405ba5a7f59a8b25404ca9c4b72022bd03
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-20 15:22:09 +0200

    Changed back-ground theme

--- client/src/components/search-bar/SearchBar.jsx
+++ client/src/components/search-bar/SearchBar.jsx
@@ -6,7 +6,7 @@ import {  category  } from '../header-data/HeaderData'
 
 const SearchBar = ({ open, setOpen }) => {
   return (
-    <div className={'w-full h-10 bg-slate-400'}>
+    <div className={'w-full h-10 bg-neutral-800'}>
       <div className={'flex justify-around '}>
         <div className={'flex p-1'}>
           <div class="relative">
@@ -14,7 +14,7 @@ const SearchBar = ({ open, setOpen }) => {
               <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
             <input type="search" id="search" class="block w-full p-1 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required/>
-            <button type="submit" class="text-white absolute h-6 right-2.5 bottom-1 bg-slate-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <button type="submit" class="text-white absolute h-6 right-2.5 bottom-1 bg-neutral-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
           </div>
         </div>
         <div onClick={() => setOpen((prev) => !prev)} className={'flex gap-x-2 p-2 text-[#fff] ml-5'}>
@@ -27,7 +27,7 @@ const SearchBar = ({ open, setOpen }) => {
           )}
           {open && (
             <div 
-              className={'bg-slate-700 absolute top-28  rounded-lg p-1 w-[150px] mr-2'}
+              className={'bg-neutral-800 absolute top-28  rounded-lg p-1 w-[150px] mr-2'}
             >
               <div className={'w-full -justify-between'}>
                 {category.map((item, index) => {
