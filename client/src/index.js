90b557a5dd89da97e963f9ede35ea4017d3206f8
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-02-27 21:16:47 +0200

    Updated file extensions

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css'
+import './index.scss'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
