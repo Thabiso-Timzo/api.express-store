c6a4bc3a976b55e42cc9143446f1967d7931c096
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-03-03 19:23:51 +0200

    Designed the mobile view of the UI

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { MdCategory, MdArrowDropUp, MdArrowDropDown } from 'react-icons/md';
 
 import './Header.scss'
 import logo from '../../assets/logo.png'
 import WebHeader from '../web-header/WebHeader';
 import Search from '../search/Search';
+import { navLinks2 } from '../nav-links/Nav-links'
+
+const category = ["Electronics", "Furniture", "Kitchen-ware"]
 
 const Header = () => {
+  const [value, setValue] = useState('Shop Category')
+  const [open, setOpen] = useState(false)
+
+  const onElementClicked = (string) => {
+    setValue(string)
+  }
+
+  const onButton = () => {
+    setOpen(prevOpen => !prevOpen)
+  }
+
   return (
     <div className='header-wrapper'>
     <div className='header-container'>
@@ -16,14 +31,46 @@ const Header = () => {
           </Link>
         </div>
         <div>
-        <Search />
+          <Search /> 
         </div>
         <div className='right-container'>
           <WebHeader />
         </div>
       </div>
       <div className="secondary-container">
-
+        <div className="secondary-wrapper">
+          <div className="drop-dwown">
+            <div className="button-group">
+              <MdCategory size={30} />
+              <button onClick={() => onButton()}>
+                <p>{value}</p>
+                <span>{open ? <MdArrowDropUp size={20} /> : <MdArrowDropDown size={20} />}</span>
+              </button>
+            </div>
+            {
+              open && <div className="content">
+              {
+                category.map((item, index) => {
+                  return (
+                    <p key={index} onClick={() => onElementClicked(item)}>{item}</p>
+                  )
+                })
+              }
+            </div>
+            }
+          </div>
+          <div className="navigation">
+            <ul>
+              {navLinks2.map((page, index) => {
+                return (
+                  <Link to={page.path}>
+                    <li>{page.title}</li>
+                  </Link>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
