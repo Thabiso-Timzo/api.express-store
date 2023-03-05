a8ea73ce078dabaf029d657c6ea2bb5422ee8da6
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-03-05 15:01:47 +0200

    Moved all the navigation components into one folder

---
+++ client/src/components/navigation-bar/web-header/WebHeader.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { GoThreeBars } from 'react-icons/go'
+
+import './WebHeader.scss'
+import { navLinks } from '../nav-links/Nav-links'
+
+const WebHeader = () => {
+  return (
+    <div className='wrapper'>
+    <div className='nav-container'>
+        <ul>
+            {navLinks.map((items, i) => {
+                return (
+                    <div key={i}>
+                        <Link to={items.path}>
+                            <li>
+                                <div className='item-wrapper'>
+                                    <span>{items.icon}</span>
+                                    <span>{items.title}<br />{items.title2}</span>
+                                </div>
+                            </li>
+                        </Link>
+                    </div>   
+                )
+            })}
+        </ul>
+    </div>
+    <div className="bars">
+            <GoThreeBars size={30} />
+        </div>
+  </div>
+  )
+}
+
+export default WebHeader
