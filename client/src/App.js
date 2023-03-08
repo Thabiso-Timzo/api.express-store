e0b78c1acac4be71a41e614c23f244340ea6eada
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-03-08 11:06:10 +0200

    Updated App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,13 @@ import Products from './pages/screens/products/Products'
 import Login from './pages/auth/login/Login'
 import Register from './pages/auth/register/Register'
 import Error404 from './pages/errors/404/404error';
+import Compare from './pages/screens/compare/Compare';
+import Wishlist from './pages/screens/wishlist/Wishlist';
+import Main from './pages/auth/main/Main';
+import Cart from './pages/screens/cart/Cart';
+import Store from './pages/screens/store/Store';
+import Blog from './pages/screens/blog/Blog'
+import Forgot from './pages/auth/forgot-password/Forgot';
 
 function App() {
   return (
@@ -17,11 +24,18 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='login' element={<Login />}/>
-          <Route path='register' element={<Register />}/>
+          <Route path='auth/login' element={<Login />}/>
+          <Route path='auth/register' element={<Register />}/>
+          <Route path='auth/forgot' element={<Forgot />}/>
           <Route path='about' element={<About />}/>
           <Route path='contact' element={<Contact />}/>
           <Route path='products' element={<Products />}/>
+          <Route path='compare' element={<Compare />} />
+          <Route path='wishlist' element={<Wishlist />} />
+          <Route path='auth' element={<Main />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='store' element={<Store />} />
+          <Route path='blog' element={<Blog />} />
           <Route path='*' element={<Error404 />}/>
         </Route>
       </Routes>
