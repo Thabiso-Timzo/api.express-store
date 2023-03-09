1653d74f4be0a8f2d43495dc6deae60706584655
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-03-09 13:57:44 +0200

    Updated footer path

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Footer from '../footer/Footer'
+import Footer from '../footer/main/Footer'
 import Header from '../navigation-bar/Header/Header'
 
 const Layout = () => {
