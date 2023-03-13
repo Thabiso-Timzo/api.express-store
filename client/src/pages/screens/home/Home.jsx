794c5647b4e6236eb1740cf63442ef87c3493caa
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-03-13 20:24:25 +0200

    Updated home UI

--- client/src/pages/screens/home/Home.jsx
+++ client/src/pages/screens/home/Home.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 import './Home.scss'
 import tv1 from '../../../assets/items/tv1.png'
 import laptop1 from '../../../assets/items/laptop1.png'
+import { options } from '../../../components/home/options/options'
 
 const Home = () => {
   return (
     <div className='container'>
       <section>
+
+        {/* promotions */}
         <div className="grid-container">
           <div className="item1">
             <div className="left">
@@ -21,28 +24,61 @@ const Home = () => {
             </div>
           </div>
           <div className="item2">
-            <div className="left"></div>
+            <div className="left">
+              <p className='best-sale'>Best sale</p>
+              <h4>Laptop Max</h4>
+              <p className='price-range'>R5678</p>
+            </div>
             <div className="right">
               <img src={laptop1} alt="" />
             </div>
           </div>
           <div className="item3">
-            <div className="left"></div>
+            <div className="left">
+              <p className='best-sale'>Best sale</p>
+              <h4>Laptop Max</h4>
+              <p className='price-range'>R5678</p>
+            </div>
             <div className="right">
               <img src={laptop1} alt="" />
             </div>
           </div>  
           <div className="item4">
-            <div className="left"></div>
+            <div className="left">
+              <p className='best-sale'>Best sale</p>
+              <h4>Laptop Max</h4>
+              <p className='price-range'>R5678</p>
+            </div>
             <div className="right">
               <img src={laptop1} alt="" />
             </div>
           </div>
           <div className="item5">
-            <div className="left"></div>
+            <div className="left">
+              <p className='best-sale'>Best sale</p>
+              <h4>Laptop Max</h4>
+              <p className='price-range'>R5678</p>
+            </div>
             <div className="right">
               <img src={laptop1} alt="" />
             </div>
+          </div>
+        </div>
+
+        {/*  */}
+        <div className="products">
+          <div className="options">
+            {options.map((option, index) => {
+              return (
+                <div className="option" key={index}>
+                  <div className="icon">{option.icon}</div>
+                  <div className="text-group">
+                    <p>{option.title}</p>
+                    <p>{option.phrase}</p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </section>
