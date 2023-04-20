4a2ca0699cbcc13cfb648e9b324fd7c0d09d33f1
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-04-20 19:37:31 +0200

    Modified file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Layout from './components/layout/Layout';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
+import Cart from './pages/cart/Cart';
+import WishList from './pages/wishlist/WishList';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='wishlist' element={<WishList />} />
         </Route>
       </Routes>
     </BrowserRouter>
