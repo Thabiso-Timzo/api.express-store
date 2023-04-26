4c615d2318eb9f8b2cea7668db2b840e88a244fa
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-04-26 16:37:01 +0200

    Added navigation links

---
+++ client/src/components/links/NavigationLinks.ts
@@ -0,0 +1,24 @@
+import { Navigation } from "../../types/types";
+
+export const navigation: Navigation[] = [
+    {
+        id: 1,
+        url: '/',
+        name: 'Home',
+    
+    },
+    {
+        id: 2,
+        url: 'store',
+        name: 'Our store',
+    },
+    {
+        id: 3,
+        url: 'blog',
+        name: 'Blog',
+    },{
+        id: 4,
+        url: 'contact',
+        name: 'Contact',
+    }
+]
