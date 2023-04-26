36dfb3712793d5a1f56ef86074b90e0be547899e
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-04-26 16:23:37 +0200

    Modified App.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,11 @@ import Contact from './pages/contact/Contact';
 import Cart from './pages/cart/Cart';
 import WishList from './pages/wishlist/WishList';
 import Error404 from './pages/404/Error404';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Compare from './pages/compare/Compare';
+import Store from './pages/store/Store';
+import Blog from './pages/blog/Blog';
 
 function App() {
   return (
@@ -20,6 +25,11 @@ function App() {
           <Route path='contact' element={<Contact />} />
           <Route path='cart' element={<Cart />} />
           <Route path='wishlist' element={<WishList />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+          <Route path='compare' element={<Compare />} />
+          <Route path='store' element={<Store />} />
+          <Route path='blog' element={<Blog />} />
           <Route path='*' element={<Error404 />} />
         </Route>
       </Routes>
