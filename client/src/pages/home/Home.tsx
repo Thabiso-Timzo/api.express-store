a25d12ec43e793bdffed0eb1f408df172f56c041
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-04 17:30:17 +0200

    Updated home page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Banner from '../../components/banner/Banner'
 import Offers from '../../components/offers/Offers'
 import Marque from '../../components/marque/Marque'
+import Blog from '../../components/blog/Blog'
+import Product from '../../components/product/Product'
 
 const Home = () => {
   return (
@@ -10,6 +12,8 @@ const Home = () => {
       <Banner />
       <Offers />
       <Marque />
+      <Product />
+      <Blog />
     </>
   )
 }
