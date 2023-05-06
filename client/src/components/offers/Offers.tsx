c9ef74f221954daf7e72ff45dace57f2c0fe9867
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-06 12:39:36 +0200

    Modified offers compoent

--- client/src/components/offers/Offers.tsx
+++ client/src/components/offers/Offers.tsx
@@ -1,52 +1,62 @@
 import React from 'react'
 
-import { services } from '../data/ServiceData'
-import { items } from '../data/ItemsData'
+type OffersProps = {
+    services: Array<any>;
+    items: Array<any>
+}
 
-const Offers = () => {
+const Offers = ({ items, services }: OffersProps ) => {
   return (
     <section className='offers'>
-        <div className="container">
-            <div className={window.innerWidth > 768 ? "services": "services-scroll"}>
-                {services.map(service => (
-                    <div className={window.innerWidth > 768 ? "services-offer" : "small"} key={service.id}>
-                        <service.icon className='icon'/>
-                        <div className="content">
-                            <h5>{service.desc}</h5>
-                            <p>{service.text}</p>
-                        </div>
-                    </div>        
-                ))}
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <div className="services">
+                        {services.map(service => (
+                            <div className="services-offer">
+                                <service.icon className="icon" />
+                                <div className="content">
+                                    <h5>{service.desc}</h5>
+                                    <p>{service.text}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-            <div className="items">
-                <div className="products">
-                    {items.map(item => (
-                        <div className="product">
-                            <div className="content">
-                                <h5>{item.desc}</h5>
-                                <p>{item.text}</p>
-                            </div>
-                            <div className="image">
-                                <img src={item.image} alt="product" />
-                            </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className="items">
+                        <div className="products">
+                            {items.map(item => (
+                                <div className="product">
+                                    <div className="content">
+                                        <h5>{item.desc}</h5>
+                                        <p>{item.text}</p>
+                                    </div>
+                                    <div className="image">
+                                        <img src={item.image} alt="product" />
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-                <hr />
-                <div className="products">
-                    {items.map(item => (
-                        <div className="product">
-                            <div className="content">
-                                <h5>{item.desc}</h5>
-                                <p>{item.text}</p>
-                            </div>
-                            <div className="image">
-                                <img src={item.image} alt="product" />
-                            </div>
+                        <hr />
+                        <div className="products">
+                            {items.map(item => (
+                                <div className="product">
+                                    <div className="content">
+                                        <h5>{item.desc}</h5>
+                                        <p>{item.text}</p>
+                                    </div>
+                                    <div className="image">
+                                        <img src={item.image} alt="product" />
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
-            </div> 
+            </div>
         </div>
     </section>
   )
