303e8df06a6403c1feaf744175dc2ccd020fbaca
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-06 15:42:47 +0200

    Modified banner UI

--- client/src/components/banner/Banner.tsx
+++ client/src/components/banner/Banner.tsx
@@ -7,55 +7,57 @@ const Banner = () => {
 
   return (
     <section className='banner'>
-        <div className={window.innerWidth > 768 ? "big-container" : "small-container"}>
-          <div className={window.innerWidth > 768 ?"image-box-1" : "image-box-2"}>
-            <img className="image" src={banner} alt="" />
-            <div className={window.innerWidth > 768 ? "content-1" : "content-2"}>
-              <h4>Laptop</h4> 
-              <h5>15 Inch laptop</h5>
-              <p>R6700</p>
-              <Link to='#' className={window.innerWidth > 768 ? 'button-1': 'button-2'}>Buy Now</Link>
+       <div className="container-xxl">
+          <div className="image-container">
+            <div className="main-image">
+                <img src={banner} alt="" />
+                <div className="container">
+                  <h4>Thabiso</h4>
+                  <h5>Hlatshwayo</h5>
+                  <p>is running</p>
+                  <Link to='#'>Buy now</Link>
+                </div>
             </div>
+            <div className="image">
+                <div className="image-row">
+                  <div className="image-wrapper">
+                    <img src={banner} alt="" />
+                    <div className="container">
+                      <h4>Thabiso</h4>
+                      <h5>Hlatshwayo</h5>
+                      <p>is running</p>
+                    </div>
+                  </div>
+                  <div className="image-wrapper">
+                    <img src={banner} alt="" />
+                    <div className="container">
+                      <h4>Thabiso</h4>
+                      <h5>Hlatshwayo</h5>
+                      <p>is running</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="image-row">
+                  <div className="image-wrapper">
+                    <img src={banner} alt="" />
+                    <div className="container">
+                      <h4>Thabiso</h4>
+                      <h5>Hlatshwayo</h5>
+                      <p>is running</p>
+                    </div>
+                  </div>
+                  <div className="image-wrapper">
+                    <img src={banner} alt="" />
+                    <div className="container">
+                      <h4>Thabiso</h4>
+                      <h5>Hlatshwayo</h5>
+                      <p>is running</p>
+                    </div>
+                  </div>
+                </div>
+            </div>s
           </div>
-          <div className="images">
-            <div className={window.innerWidth > 768 ? "image-row-1": "image-row-2"}>
-              <div className={window.innerWidth > 768 ?"image-box-1" : "image-box-2"}>
-                <img className="image-1" src={banner} alt="" />
-                <div className={window.innerWidth > 768 ? "content-1" : "content-2"}>
-                  <h4>Laptop</h4> 
-                  <h5>15 Inch laptop</h5>
-                  <p>R6700</p>
-                </div>
-              </div>
-              <div className={window.innerWidth > 768 ?"image-box-1" : "image-box-2"}>
-                <img className="image-2" src={banner} alt="" />
-                <div className={window.innerWidth > 768 ? "content-1" : "content-2"}>
-                  <h4>Laptop</h4> 
-                  <h5>15 Inch laptop</h5>
-                  <p>R6700</p>
-                </div>
-              </div>
-            </div>
-            <div className={window.innerWidth > 768 ? "image-row-1": "image-row-2"}>
-              <div className={window.innerWidth > 768 ?"image-box-1" : "image-box-2"}>
-                <img className="image-3" src={banner} alt="" />
-                <div className={window.innerWidth > 768 ? "content-1" : "content-2"}>
-                  <h4>Laptop</h4> 
-                  <h5>15 Inch laptop</h5>
-                  <p>R6700</p>
-                </div>
-              </div>
-              <div className={window.innerWidth > 768 ?"image-box-1" : "image-box-2"}>
-                <img className="image-4" src={banner} alt="" />
-                <div className={window.innerWidth > 768 ? "content-1" : "content-2"}>
-                  <h4>Laptop</h4> 
-                  <h5>15 Inch laptop</h5>
-                  <p>R6700</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       </div>
     </section>
   )
 }
