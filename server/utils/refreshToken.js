2304470372d9f0d546d86607ef43e68c4e5cf48b
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2023-05-25 06:28:17 +0200

    Removed all the folders

--- server/utils/refreshToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const { jwt_secret } = require('../config/env/index')
-
-const generateRefreshToken = (id) => {
-    return jwt.sign({ id }, jwt_secret, { expiresIn: '3d'})
-}
-
-module.exports = { generateRefreshToken }
