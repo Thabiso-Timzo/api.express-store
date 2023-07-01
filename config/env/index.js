7ec747a4f424e3d9eccb3043b4af6352c12d8377
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-07-01 16:23:00 +0200

    Updated

--- config/env/index.js
+++ config/env/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     server_port: process.env.PORT,
-    db_connection: process.env.LOCAL_MONGO_URI,
+    db_connection: process.env.MONGO_URI,
     jwt_secret: process.env.JWT_SECRET,
     email_address: process.env.EMAIL_ADDRESS,
     email_password: process.env.EMAIL_PASSWORD,
